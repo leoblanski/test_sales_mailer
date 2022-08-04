dc118ec9934b8b31c4dc80dc7ca2427fac364e9a
Leonardo Blanski <leo_blanski@hotmail.com>
2022-08-03 21:53:56 -0300

    fix: adding percentage to employee migration

--- database/migrations/2022_08_04_004511_create_employees_table.php
+++ database/migrations/2022_08_04_004511_create_employees_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->nullable();
+            $table->float('comission_percentage')->nullable();
             $table->timestamps();
         });
     }
