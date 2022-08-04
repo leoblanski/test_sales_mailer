9157a3cef60cfb8072f2fef3fcf328321ee7da7b
Leonardo Blanski <leo_blanski@hotmail.com>
2022-08-03 22:33:38 -0300

    add: order crud

---
+++ database/migrations/2022_08_04_004828_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id')->nullable();
+            $table->decimal('amount', 8, 2)->default(0);
+            $table->decimal('commission', 8, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['employee_id']);
+        });
+
+        Schema::dropIfExists('orders');
+    }
+};
